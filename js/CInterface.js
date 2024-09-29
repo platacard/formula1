95c921890f4f7d1e512dca4cbfeca926e25b50b9
parabolabam <sorokinvladislav1402@gmail.com>
2024-09-29 14:54:01 +0400

    feat: add timer, but with wrong calculations

--- js/CInterface.js
+++ js/CInterface.js
@@ -30,195 +30,309 @@ function CInterface(){
     var _oRankContainer;
     var _oRankIndicator;
     var _pStartRankPos;
-    
-    this._init = function(){          
-        var aButtonList = new Array();
-        
-        var oInterfaceContainer = new createjs.Container();
-        s_oStage.addChild(oInterfaceContainer);
-        
-        var oSprite = s_oSpriteLibrary.getSprite('but_exit');
-        _oButExit = new CGfxButton(0, 0, oSprite, oInterfaceContainer);
-        _oButExit.addEventListener(ON_MOUSE_UP, this._onExit, this);
-        
-        if(DISABLE_SOUND_MOBILE === false || s_bMobile === false){
-            var oSprite = s_oSpriteLibrary.getSprite('audio_icon');
-            _oAudioToggle = new CToggle(0,0,oSprite,s_bAudioActive);
-            _oAudioToggle.addEventListener(ON_MOUSE_UP, this._onAudioToggle, this);  
-        }
-        
-        var doc = window.document;
-        var docEl = doc.documentElement;
-        _fRequestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-        _fCancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-        if(ENABLE_FULLSCREEN === false){
-            _fRequestFullScreen = false;
-        }
-
-        if (_fRequestFullScreen && screenfull.isEnabled){
-            oSprite = s_oSpriteLibrary.getSprite("but_fullscreen")
-            _oButFullscreen = new CToggle(0,0,oSprite,s_bFullscreen, oInterfaceContainer);
-            _oButFullscreen.addEventListener(ON_MOUSE_UP,this._onFullscreenRelease,this);
-            aButtonList.push(_oButFullscreen);
-        }
-
-        var oSprite = s_oSpriteLibrary.getSprite('but_settings');
-        _pStartPosGUI = {x:CANVAS_WIDTH - (oSprite.width/2) -10,y:(oSprite.height/2) +10};
-        _oGUIExpandible = new CGUIExpandible(_pStartPosGUI.x, _pStartPosGUI.y, oSprite, oInterfaceContainer);
-        _oGUIExpandible.addButton(_oButExit);
-        
-        if(DISABLE_SOUND_MOBILE === false || s_bMobile === false){
-            _oGUIExpandible.addButton(_oAudioToggle);
-        }
-        
-        if (_fRequestFullScreen && screenfull.isEnabled){
-            _oGUIExpandible.addButton(_oButFullscreen);
-        }
-
-
-        ///////SET SPEED INDICATOR
-        _oTachometer = new CTachometer(206, CANVAS_HEIGHT-12, oInterfaceContainer);
-
-        ///////SET MINIMAP
-        _pStartPosMiniMap = {x: CANVAS_WIDTH-162, y: CANVAS_HEIGHT-226};
-        _oMiniMap = new CMiniMap(_pStartPosMiniMap.x, _pStartPosMiniMap.y, oInterfaceContainer);
-        _oMiniMap.setPath(s_oGame.getSegments());
-
-        ///////LAP INDICATOR
-        _oLapIndicator = new CLapIndicator(CANVAS_WIDTH/2, 10, s_oStage);
-
-        ///////RANK INDICATOR
-        _pStartRankPos = {x:20, y:0}
-        _oRankContainer = new createjs.Container();
-        _oRankContainer.x = _pStartRankPos.x;
-        _oRankContainer.y = _pStartRankPos.y;
-        oInterfaceContainer.addChild(_oRankContainer);
-
-        
-        _oRankIndicator = new CRankIndicator(150, 70, _oRankContainer, 150);
-
-        _oCountdownTextStroke = new createjs.Text(""," 300px "+PRIMARY_FONT, "#000");
-        _oCountdownTextStroke.x = CANVAS_WIDTH/2;
-        _oCountdownTextStroke.y = CANVAS_HEIGHT/2;
-        _oCountdownTextStroke.textAlign = "center";
-        _oCountdownTextStroke.textBaseline = "middle";
-        _oCountdownTextStroke.lineWidth = 200;
-        _oCountdownTextStroke.outline = 20;
-        oInterfaceContainer.addChild(_oCountdownTextStroke);
-
-        _oCountdownText = new createjs.Text(""," 300px "+PRIMARY_FONT, "#fff");
-        _oCountdownText.x = CANVAS_WIDTH/2;
-        _oCountdownText.y = CANVAS_HEIGHT/2;
-        _oCountdownText.textAlign = "center";
-        _oCountdownText.textBaseline = "middle";
-        _oCountdownText.lineWidth = 200;
-        oInterfaceContainer.addChild(_oCountdownText);
-
-        if(s_bMobile){
-            var iY = CANVAS_HEIGHT-300;
-            var iX = 180;
-            var scaleFactor = 1;  // Коэффициент увеличения
-            
-            ///////////// MOVEMENT
-            var oSprite = s_oSpriteLibrary.getSprite('key_up');
-            _pStartPosUp = {x: CANVAS_WIDTH-iX +oSprite.width/2, y: iY-350};
-            _oButUp = new CGfxButton(_pStartPosUp.x, _pStartPosUp.y, oSprite, oInterfaceContainer);
-            _oButUp.addEventListenerWithParams(ON_MOUSE_UP, s_oGame.onKeyUp, s_oGame, KEY_UP);
-            _oButUp.addEventListenerWithParams(ON_MOUSE_DOWN, s_oGame.onKeyDown, s_oGame, KEY_UP);
-            
-            var oSprite = s_oSpriteLibrary.getSprite('key_down');
-            _pStartPosDown = {x: CANVAS_WIDTH / 2 , y: CANVAS_HEIGHT - oSprite.height +100};  // Центр экрана по X
-            _oButDown = new CGfxButton(_pStartPosDown.x, _pStartPosDown.y, oSprite, oInterfaceContainer);
-            _oButDown.addEventListenerWithParams(ON_MOUSE_UP, s_oGame.onKeyUp, s_oGame, KEY_DOWN);
-            _oButDown.addEventListenerWithParams(ON_MOUSE_DOWN, s_oGame.onKeyDown, s_oGame, KEY_DOWN);
-            
-            ///////////// DIRECTION
-            var oSprite = s_oSpriteLibrary.getSprite('key_left');
-            oSprite.scaleX = scaleFactor;  // Увеличиваем ширину
-oSprite.scaleY = scaleFactor;  // Увеличиваем высоту
-            _pStartPosLeft = {x:iX-oSprite.width/2, y: iY-160};
-            _oButLeft = new CGfxButton(_pStartPosLeft.x, _pStartPosLeft.y, oSprite, oInterfaceContainer);
-            _oButLeft.addEventListenerWithParams(ON_MOUSE_UP, s_oGame.onKeyUp, s_oGame, KEY_LEFT);
-            _oButLeft.addEventListenerWithParams(ON_MOUSE_DOWN, s_oGame.onKeyDown, s_oGame, KEY_LEFT);
-            
-            var oSprite = s_oSpriteLibrary.getSprite('key_right');
-            _pStartPosRight = {x: CANVAS_WIDTH-iX +oSprite.width/2, y: iY-160};
-            _oButRight = new CGfxButton(_pStartPosRight.x, _pStartPosRight.y, oSprite, oInterfaceContainer);
-            _oButRight.addEventListenerWithParams(ON_MOUSE_UP, s_oGame.onKeyUp, s_oGame, KEY_RIGHT);
-            _oButRight.addEventListenerWithParams(ON_MOUSE_DOWN, s_oGame.onKeyDown, s_oGame, KEY_RIGHT);
-            
-        }
-
-        _aNumCountDown = new Array();
-        for(var i=0; i<=3; i++){
-            _aNumCountDown[i] = false;
-        }
-
-        this.refreshButtonPos();
-    };
-    
-    this.unload = function(){
-        _oGUIExpandible.unload();
-
-        s_oInterface = null;
-        
-        if(s_bMobile){
-            _oButUp.unload();
-            _oButDown.unload();
-            _oButLeft.unload();
-            _oButRight.unload();
-        }
-        
-    };
-    
-    this.refreshButtonPos = function(){
-        _oGUIExpandible.refreshPos();
-
-        if(s_bMobile){
- 
-            _oButDown.setPosition(_pStartPosDown.x, _pStartPosDown.y);  // Убираем оффсеты для кнопки "тормоз"
-            _oButLeft.setPosition(_pStartPosLeft.x + s_iOffsetX, _pStartPosLeft.y - s_iOffsetY);
-            _oButRight.setPosition(_pStartPosRight.x - s_iOffsetX, _pStartPosRight.y - s_iOffsetY);
-        }
-
-        _oTachometer.updateOffset(s_iOffsetX, s_iOffsetY);
-
-        _oMiniMap.setPosition(_pStartPosMiniMap.x- s_iOffsetX, _pStartPosMiniMap.y - s_iOffsetY);
-        
-        _oLapIndicator.setPosition();
-
-        _oRankContainer.x = _pStartRankPos.x + s_iOffsetX;
-        _oRankContainer.y = _pStartRankPos.y + s_iOffsetY;
-        
-        
-        
-    };
-    
-    this.resetFullscreenBut = function(){
-	if (_fRequestFullScreen && screenfull.isEnabled){
-		_oButFullscreen.setActive(s_bFullscreen);
-	}
+    let raceTime = {
+      prev: 0,
+      curr: 0,
     };
 
-        
-    this._onFullscreenRelease = function(){
-	if(s_bFullscreen) { 
-		_fCancelFullScreen.call(window.document);
-	}else{
-		_fRequestFullScreen.call(window.document.documentElement);
-	}
-	
-	sizeHandler();
+    this._init = function () {
+      var aButtonList = new Array();
+
+      var oInterfaceContainer = new createjs.Container();
+      s_oStage.addChild(oInterfaceContainer);
+
+      var oSprite = s_oSpriteLibrary.getSprite("but_exit");
+      _oButExit = new CGfxButton(0, 0, oSprite, oInterfaceContainer);
+      _oButExit.addEventListener(ON_MOUSE_UP, this._onExit, this);
+
+      if (DISABLE_SOUND_MOBILE === false || s_bMobile === false) {
+        var oSprite = s_oSpriteLibrary.getSprite("audio_icon");
+        _oAudioToggle = new CToggle(0, 0, oSprite, s_bAudioActive);
+        _oAudioToggle.addEventListener(ON_MOUSE_UP, this._onAudioToggle, this);
+      }
+
+      var doc = window.document;
+      var docEl = doc.documentElement;
+      _fRequestFullScreen =
+        docEl.requestFullscreen ||
+        docEl.mozRequestFullScreen ||
+        docEl.webkitRequestFullScreen ||
+        docEl.msRequestFullscreen;
+      _fCancelFullScreen =
+        doc.exitFullscreen ||
+        doc.mozCancelFullScreen ||
+        doc.webkitExitFullscreen ||
+        doc.msExitFullscreen;
+
+      if (ENABLE_FULLSCREEN === false) {
+        _fRequestFullScreen = false;
+      }
+
+      if (_fRequestFullScreen && screenfull.isEnabled) {
+        oSprite = s_oSpriteLibrary.getSprite("but_fullscreen");
+        _oButFullscreen = new CToggle(
+          0,
+          0,
+          oSprite,
+          s_bFullscreen,
+          oInterfaceContainer
+        );
+        _oButFullscreen.addEventListener(
+          ON_MOUSE_UP,
+          this._onFullscreenRelease,
+          this
+        );
+        aButtonList.push(_oButFullscreen);
+      }
+
+      var oSprite = s_oSpriteLibrary.getSprite("but_settings");
+      _pStartPosGUI = {
+        x: CANVAS_WIDTH - oSprite.width / 2 - 10,
+        y: oSprite.height / 2 + 10,
+      };
+      _oGUIExpandible = new CGUIExpandible(
+        _pStartPosGUI.x,
+        _pStartPosGUI.y,
+        oSprite,
+        oInterfaceContainer
+      );
+      _oGUIExpandible.addButton(_oButExit);
+
+      if (DISABLE_SOUND_MOBILE === false || s_bMobile === false) {
+        _oGUIExpandible.addButton(_oAudioToggle);
+      }
+
+      if (_fRequestFullScreen && screenfull.isEnabled) {
+        _oGUIExpandible.addButton(_oButFullscreen);
+      }
+
+      ///////SET SPEED INDICATOR
+      _oTachometer = new CTachometer(
+        206,
+        CANVAS_HEIGHT - 12,
+        oInterfaceContainer
+      );
+
+      ///////SET MINIMAP
+      _pStartPosMiniMap = { x: CANVAS_WIDTH - 162, y: CANVAS_HEIGHT - 226 };
+      _oMiniMap = new CMiniMap(
+        _pStartPosMiniMap.x,
+        _pStartPosMiniMap.y,
+        oInterfaceContainer
+      );
+      _oMiniMap.setPath(s_oGame.getSegments());
+
+      ///////LAP INDICATOR
+      _oLapIndicator = new CLapIndicator(CANVAS_WIDTH / 2, 10, s_oStage);
+
+      ///////RANK INDICATOR
+      _pStartRankPos = { x: 20, y: 0 };
+      _oRankContainer = new createjs.Container();
+      _oRankContainer.x = _pStartRankPos.x;
+      _oRankContainer.y = _pStartRankPos.y;
+      oInterfaceContainer.addChild(_oRankContainer);
+
+      _oRankIndicator = new CRankIndicator(150, 70, _oRankContainer, 150);
+
+      _oCountdownTextStroke = new createjs.Text(
+        "",
+        " 300px " + PRIMARY_FONT,
+        "#000"
+      );
+      _oCountdownTextStroke.x = CANVAS_WIDTH / 2;
+      _oCountdownTextStroke.y = CANVAS_HEIGHT / 2;
+      _oCountdownTextStroke.textAlign = "center";
+      _oCountdownTextStroke.textBaseline = "middle";
+      _oCountdownTextStroke.lineWidth = 200;
+      _oCountdownTextStroke.outline = 20;
+      oInterfaceContainer.addChild(_oCountdownTextStroke);
+
+      _oCountdownText = new createjs.Text("", " 300px " + PRIMARY_FONT, "#fff");
+      _oCountdownText.x = CANVAS_WIDTH / 2;
+      _oCountdownText.y = CANVAS_HEIGHT / 2;
+      _oCountdownText.textAlign = "center";
+      _oCountdownText.textBaseline = "middle";
+      _oCountdownText.lineWidth = 200;
+      oInterfaceContainer.addChild(_oCountdownText);
+
+      if (s_bMobile) {
+        var iY = CANVAS_HEIGHT - 300;
+        var iX = 180;
+        var scaleFactor = 1; // Коэффициент увеличения
+
+        ///////////// MOVEMENT
+        var oSprite = s_oSpriteLibrary.getSprite("key_up");
+        _pStartPosUp = {
+          x: CANVAS_WIDTH - iX + oSprite.width / 2,
+          y: iY - 350,
+        };
+        _oButUp = new CGfxButton(
+          _pStartPosUp.x,
+          _pStartPosUp.y,
+          oSprite,
+          oInterfaceContainer
+        );
+        _oButUp.addEventListenerWithParams(
+          ON_MOUSE_UP,
+          s_oGame.onKeyUp,
+          s_oGame,
+          KEY_UP
+        );
+        _oButUp.addEventListenerWithParams(
+          ON_MOUSE_DOWN,
+          s_oGame.onKeyDown,
+          s_oGame,
+          KEY_UP
+        );
+
+        var oSprite = s_oSpriteLibrary.getSprite("key_down");
+        _pStartPosDown = {
+          x: CANVAS_WIDTH / 2,
+          y: CANVAS_HEIGHT - oSprite.height + 100,
+        }; // Центр экрана по X
+        _oButDown = new CGfxButton(
+          _pStartPosDown.x,
+          _pStartPosDown.y,
+          oSprite,
+          oInterfaceContainer
+        );
+        _oButDown.addEventListenerWithParams(
+          ON_MOUSE_UP,
+          s_oGame.onKeyUp,
+          s_oGame,
+          KEY_DOWN
+        );
+        _oButDown.addEventListenerWithParams(
+          ON_MOUSE_DOWN,
+          s_oGame.onKeyDown,
+          s_oGame,
+          KEY_DOWN
+        );
+
+        ///////////// DIRECTION
+        var oSprite = s_oSpriteLibrary.getSprite("key_left");
+        oSprite.scaleX = scaleFactor; // Увеличиваем ширину
+        oSprite.scaleY = scaleFactor; // Увеличиваем высоту
+        _pStartPosLeft = { x: iX - oSprite.width / 2, y: iY - 160 };
+        _oButLeft = new CGfxButton(
+          _pStartPosLeft.x,
+          _pStartPosLeft.y,
+          oSprite,
+          oInterfaceContainer
+        );
+        _oButLeft.addEventListenerWithParams(
+          ON_MOUSE_UP,
+          s_oGame.onKeyUp,
+          s_oGame,
+          KEY_LEFT
+        );
+        _oButLeft.addEventListenerWithParams(
+          ON_MOUSE_DOWN,
+          s_oGame.onKeyDown,
+          s_oGame,
+          KEY_LEFT
+        );
+
+        var oSprite = s_oSpriteLibrary.getSprite("key_right");
+        _pStartPosRight = {
+          x: CANVAS_WIDTH - iX + oSprite.width / 2,
+          y: iY - 160,
+        };
+        _oButRight = new CGfxButton(
+          _pStartPosRight.x,
+          _pStartPosRight.y,
+          oSprite,
+          oInterfaceContainer
+        );
+        _oButRight.addEventListenerWithParams(
+          ON_MOUSE_UP,
+          s_oGame.onKeyUp,
+          s_oGame,
+          KEY_RIGHT
+        );
+        _oButRight.addEventListenerWithParams(
+          ON_MOUSE_DOWN,
+          s_oGame.onKeyDown,
+          s_oGame,
+          KEY_RIGHT
+        );
+      }
+
+      _aNumCountDown = new Array();
+      for (var i = 0; i <= 3; i++) {
+        _aNumCountDown[i] = false;
+      }
+
+      this.refreshButtonPos();
     };
 
+    this.unload = function () {
+      _oGUIExpandible.unload();
 
-    this.refreshFPS = function(iValue){
-        _oTimer.setTime(iValue);
+      s_oInterface = null;
+
+      if (s_bMobile) {
+        _oButUp.unload();
+        _oButDown.unload();
+        _oButLeft.unload();
+        _oButRight.unload();
+      }
     };
 
-    this.refreshLap = function(iCur, iTot){
-        _oLapIndicator.refreshLap(iCur, iTot);
+    this.refreshButtonPos = function () {
+      _oGUIExpandible.refreshPos();
+
+      if (s_bMobile) {
+        _oButDown.setPosition(_pStartPosDown.x, _pStartPosDown.y); // Убираем оффсеты для кнопки "тормоз"
+        _oButLeft.setPosition(
+          _pStartPosLeft.x + s_iOffsetX,
+          _pStartPosLeft.y - s_iOffsetY
+        );
+        _oButRight.setPosition(
+          _pStartPosRight.x - s_iOffsetX,
+          _pStartPosRight.y - s_iOffsetY
+        );
+      }
+
+      _oTachometer.updateOffset(s_iOffsetX, s_iOffsetY);
+
+      _oMiniMap.setPosition(
+        _pStartPosMiniMap.x - s_iOffsetX,
+        _pStartPosMiniMap.y - s_iOffsetY
+      );
+
+      _oLapIndicator.setPosition();
+
+      _oRankContainer.x = _pStartRankPos.x + s_iOffsetX;
+      _oRankContainer.y = _pStartRankPos.y + s_iOffsetY;
+    };
+
+    this.resetFullscreenBut = function () {
+      if (_fRequestFullScreen && screenfull.isEnabled) {
+        _oButFullscreen.setActive(s_bFullscreen);
+      }
+    };
+
+    this._onFullscreenRelease = function () {
+      if (s_bFullscreen) {
+        _fCancelFullScreen.call(window.document);
+      } else {
+        _fRequestFullScreen.call(window.document.documentElement);
+      }
+
+      sizeHandler();
+    };
+
+    this.refreshFPS = function (iValue) {
+      _oTimer.setTime(iValue);
+    };
+
+    this.refreshLap = function (iCur, iTot) {
+      _oLapIndicator.refreshLap(iCur, iTot);
+    };
+
+    this.refreshRaceTime = function (iValue) {
+      _oLapIndicator.refreshRaceTime(iValue);
     };
 
     this.refreshRankPosition = function(iPos){
--- js/CLapIndicator.js
+++ js/CLapIndicator.js
@@ -4,47 +4,89 @@ function CLapIndicator(iX, iY, oParentContainer){
     
     var _pStartPos;
     
-    this._init = function(){
-        _pStartPos = {x: iX, y:iY};
-        
-        _oContainer = new createjs.Container();
-        _oContainer.x = iX;
-        _oContainer.y = iY;
-        oParentContainer.addChild(_oContainer);
-        
-        var oSprite = s_oSpriteLibrary.getSprite('lap_panel');
-        var oBg = createBitmap(oSprite);
-        oBg.regX = oSprite.width/2;
-        oBg.regY = 0;
-        _oContainer.addChild(oBg);
-        
-        var iWidth = 140;
-        var iHeight = 70;
-        var iTextX = 50;
-        var iTextY = 54;
-        _oText = new CTLText(_oContainer, 
-                    iTextX-iWidth/2, iTextY-iHeight/2, iWidth, iHeight, 
-                    70, "center", "#fff", PRIMARY_FONT, 1,
-                    2, 2,
-                    sprintf(TEXT_LAP, 0, 0),
-                    true, true, false,
-                    false );
-        _oText.setStroke(10,"#000");
+    this._init = function () {
+      _pStartPos = { x: iX, y: iY };
+
+      _oContainer = new createjs.Container();
+      _oContainer.x = iX;
+      _oContainer.y = iY;
+      oParentContainer.addChild(_oContainer);
+
+      var oSprite = s_oSpriteLibrary.getSprite("lap_panel");
+      var oBg = createBitmap(oSprite);
+      oBg.regX = oSprite.width / 2;
+      oBg.regY = 0;
+      _oContainer.addChild(oBg);
+
+      var iWidth = 140;
+      var iHeight = 70;
+      var iTextX = 50;
+      var iTextY = 54;
+      _oText = new CTLText(
+        _oContainer,
+        iTextX - iWidth / 2,
+        iTextY - iHeight / 2,
+        iWidth,
+        iHeight,
+        70,
+        "center",
+        "#fff",
+        PRIMARY_FONT,
+        1,
+        2,
+        2,
+        sprintf(TEXT_LAP, 0, 0),
+        true,
+        true,
+        false,
+        false
+      );
+      _oText.setStroke(10, "#000");
+
+      _oRaceTime = new CTLText(
+        _oContainer,
+        iTextX * 2.5,
+        iTextY - iHeight / 2,
+        iWidth,
+        iHeight,
+        70,
+        "center",
+        "#fff",
+        PRIMARY_FONT,
+        1,
+        2,
+        2,
+        sprintf("%s:%s", 0, 0),
+        true,
+        true,
+        false,
+        false
+      );
+      _oRaceTime.setStroke(10, "#000");
     };
-    
-    this.unload = function(){
-        oParentContainer.removeChild(_oContainer);
+
+    this.unload = function () {
+      oParentContainer.removeChild(_oContainer);
     };
-    
-    this.setPosition = function(){
-        _oContainer.x = _pStartPos.x;
-        _oContainer.y = _pStartPos.y + s_iOffsetY;
+
+    this.setPosition = function () {
+      _oContainer.x = _pStartPos.x;
+      _oContainer.y = _pStartPos.y + s_iOffsetY;
     };
-    
-    this.refreshLap = function(iCur, iTot){
-        var szText = sprintf(TEXT_LAP, iCur, iTot);
-        
-        _oText.refreshText(szText);
+
+    this.refreshLap = function (iCur, iTot) {
+      var szText = sprintf(TEXT_LAP, iCur, iTot);
+
+      _oText.refreshText(szText);
+    };
+
+    this.refreshRaceTime = function (iMilliseconds) {
+      var iSeconds = Math.floor(iMilliseconds / 1000);
+      var iMinutes = Math.floor(iSeconds / 60);
+      iSeconds -= iMinutes * 60;
+
+      var szText = sprintf("%s:%s", iMinutes, iSeconds);
+      _oRaceTime.refreshText(szText);
     };
     
     this.setVisible = function(bVal){
